f315bb18f2e26506fea10478551a10bd1da98f40
Rohit Mishra <rohit@rohitmishra.me>
2014-03-26 21:52:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+== README
+
+Hashtagger retrieves tweets having a particular hashtag from the Twitter API and shows it to you.
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hashtagger::Application.config.session_store :cookie_store, key: '_hashtagger_session'
